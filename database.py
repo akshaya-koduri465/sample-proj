8f53b542b03d987be8429efd1599cefcd925db86
akshaya-koduri465 <koduriakshaya61@gmail.com>
2026-07-30 16:09:53 +0530

    Fix DATABASE_URL string formatting

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # DATABASE_URL = "mysql+pymysql://root:akshaya@localhost:3306/products_db"
-DATABASE_URL = mysql+pymysql://avnadmin:AVNS_kJIbgT5xyVTBGDKGX_V@mysqldb-python-backend.d.aivencloud.com:26537/defaultdb
+DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_kJIbgT5xyVTBGDKGX_V@mysqldb-python-backend.d.aivencloud.com:26537/defaultdb"
 
 engine = create_engine(DATABASE_URL)
 
