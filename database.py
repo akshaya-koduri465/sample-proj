07b8577c4c80fe352b91fd1219c5554fd0b038c5
akshaya-koduri465 <koduriakshaya61@gmail.com>
2026-07-30 16:06:35 +0530

    Update DATABASE_URL for database connection

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:akshaya@localhost:3306/products_db"
+# DATABASE_URL = "mysql+pymysql://root:akshaya@localhost:3306/products_db"
+DATABASE_URL = mysql+pymysql://avnadmin:AVNS_kJIbgT5xyVTBGDKGX_V@mysqldb-python-backend.d.aivencloud.com:26537/defaultdb
 
 engine = create_engine(DATABASE_URL)
 
