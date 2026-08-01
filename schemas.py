7a27b78cfb4d230e19a4829614476df2c5d83b98
akshaya-koduri465 <koduriakshaya61@gmail.com>
2026-08-01 15:41:50 +0530

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,8 +1,7 @@
 from pydantic import BaseModel,condecimal
 from typing import Annotated #it is used to validate the data and 
 class productCreate(BaseModel):
-    category: str
-    department:str 
+    category: str 
     name:str
     brand:str
     model:str
