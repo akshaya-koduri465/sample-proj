ce29a0cf010655bdc5458564d883e1c8dee68569
akshaya-koduri465 <koduriakshaya61@gmail.com>
2026-08-02 17:11:44 +0530

    added env file

--- database.py
+++ database.py
@@ -4,16 +4,12 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 from dotenv import load_dotenv
 import os
-#DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_kJIbgT5xyVTBGDKGX_V@mysqldb-python-backend.d.aivencloud.com:26537/defaultdb" 
+#DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_kJIbgT5xyVTBGDKGX_V@mysqldb-python-backend.d.aivencloud.com:26537/defaultdb"
+#DATABASE_URL = "mysql+pymysql://root:akshaya@localhost:3306/products_db"
 load_dotenv()
 DATABASE_URL=os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={
-        "ssl": {}
-    }
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
